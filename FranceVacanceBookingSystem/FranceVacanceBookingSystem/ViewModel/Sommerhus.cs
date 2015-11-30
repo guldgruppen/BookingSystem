07222136cbee02db3d16c376edde154bff66e730
Meihlen <rickimeihlen@gmail.com>
2015-11-30 12:30:06 +0100

    Sommerhus

---
+++ FranceVacanceBookingSystem/FranceVacanceBookingSystem/ViewModel/Sommerhus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FranceVacanceBookingSystem.Model;
+
+namespace FranceVacanceBookingSystem.ViewModel
+{
+    class Sommerhus
+    {
+        private ObservableCollection<SommerhusBeskrivelse> _sommerhuse;
+
+        public ObservableCollection<SommerhusBeskrivelse> Sommerhuse
+        {
+            get
+            {
+                return _sommerhuse;
+            }
+
+            set
+            {
+                _sommerhuse = value;
+            }
+        }
+
+        public double Distancefromwater { get; set; }
+        public int Bathrooms { get; set; }
+        public int Parkinglots { get; set; }
+        public int Bedrooms { get; set; }
+        public string Location { get; set; }
+        public bool Petallowed { get; set; }
+        public double Price { get; set; }
+        public double Size { get; set; }
+
+        public Sommerhus()
+        {
+            Sommerhuse = new ObservableCollection<SommerhusBeskrivelse>();
+            Sommerhuse.Add(new SommerhusBeskrivelse(5,2,2,1,"Paris",false,5000,180));
+ 
+        }
+
+        public void AddSommerhus()
+        {
+            Sommerhuse.Add(new SommerhusBeskrivelse(Distancefromwater,Bathrooms,Parkinglots,Bedrooms, Location, Petallowed, Price, Size));
+        }
+        
+    }
+}
