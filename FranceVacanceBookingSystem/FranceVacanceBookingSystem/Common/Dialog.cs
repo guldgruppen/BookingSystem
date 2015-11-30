9e36a14f3d72a9f01bae8d1bae050483ca704d02
Kasper Due <kasp9023@hotmail.com>
2015-11-30 21:09:03 +0100

    Tilføjet en dialog hjælper :)

---
+++ FranceVacanceBookingSystem/FranceVacanceBookingSystem/Common/Dialog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Popups;
+
+namespace FranceVacanceBookingSystem.Common
+{
+    public class Dialog
+    {
+
+        public static async void Show(string text)
+        {
+            MessageDialog dialog = new MessageDialog(text);
+            await dialog.ShowAsync();
+        }
+    }
+
+        
+}
