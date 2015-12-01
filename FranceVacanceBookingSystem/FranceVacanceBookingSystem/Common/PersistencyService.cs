bb9be5e2c6ade315f20a8baf7ed5350ae23568be
Meihlen <rickimeihlen@gmail.com>
2015-12-01 10:47:41 +0100

    Sommerhusliste

--- FranceVacanceBookingSystem/FranceVacanceBookingSystem/Common/PersistencyService.cs
+++ FranceVacanceBookingSystem/FranceVacanceBookingSystem/Common/PersistencyService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.UI.Popups;
 using FranceVacanceBookingSystem.Model;
+using FranceVacanceBookingSystem.ViewModel;
 using Newtonsoft.Json;
 
 namespace FranceVacanceBookingSystem.Common
@@ -37,6 +38,14 @@ namespace FranceVacanceBookingSystem.Common
             return null;
         }
 
+        public static async Task<List<Sommerhus>> LoadSommerhusFromJsonAsync()
+        {
+            string sommerhusJsonString = await DeserializeNotesFileAsync(SommerHuse);
+            if (sommerhusJsonString != null)
+                return (List<Sommerhus>)JsonConvert.DeserializeObject(sommerhusJsonString, typeof(List<Profil>));
+            return null;
+        }
+
 
 
         private static async void SerializeNotesFileAsync(string notesJsonString, string fileName)
@@ -45,8 +54,28 @@ namespace FranceVacanceBookingSystem.Common
             await FileIO.WriteTextAsync(localFile, notesJsonString);
         }
 
+        private static async void SerializeSommerhusFileAsync(string sommerhusJsonString, string fileName)
+        {
+            StorageFile localFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(localFile, sommerhusJsonString);
+        }
+
 
         private static async Task<string> DeserializeNotesFileAsync(string fileName)
+        {
+            try
+            {
+                StorageFile localFile = await ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+                return await FileIO.ReadTextAsync(localFile);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageDialogHelper.Show("Loading for the first time? - Try Add and Save some Notes before trying to Save for the first time", "File not Found");
+                return null;
+            }
+        }
+
+        private static async Task<string> DeserializeSommerhusFileAsync(string fileName)
         {
             try
             {
