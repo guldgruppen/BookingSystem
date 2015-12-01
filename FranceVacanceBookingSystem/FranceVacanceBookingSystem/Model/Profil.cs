cbf7c4985c6faa80b38c0da24aaa4bdb6c587e72
Kasper Due <kasp9023@hotmail.com>
2015-12-01 19:10:06 +0100

    that is the good branch

--- FranceVacanceBookingSystem/FranceVacanceBookingSystem/Model/Profil.cs
+++ FranceVacanceBookingSystem/FranceVacanceBookingSystem/Model/Profil.cs
@@ -100,7 +100,7 @@ namespace FranceVacanceBookingSystem.Model
 
         public void CheckTlf(string tlf)
         {
-            if (tlf.Length != 8)
+            if (String.IsNullOrWhiteSpace(tlf) || tlf.Length != 8)
                 throw new ArgumentException("Venlig indtast et korret telefon nummer");
         }
 
@@ -108,7 +108,7 @@ namespace FranceVacanceBookingSystem.Model
         {
             if (String.IsNullOrWhiteSpace(name))
             {
-                throw new ArgumentException("Venlist indtast et ordentlig navn! WIRK!!");
+                throw new ArgumentException("Venlist indtast et ordentlig navn");
             }
         }
 
